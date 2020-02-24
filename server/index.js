02d4ac62de2290f9aa2e896f91da5b6547986800
sayertindall <34878057+sayertindall@users.noreply.github.com>
2020-02-24 11:12:00 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,27 +10,26 @@ const { client } = require('../database/PostGres.js');
 const PORT = 3004;
 
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(cors());
 app.use(express.static(`${__dirname}/../client/dist`));
 
 // Route for MongoDB
-// app.get('/reviews/:room_id', (req, res) => {
-  // mongoose.connect('mongodb://localhost:27017/airbnb', { useNewUrlParser: true });
-  // const target = {room_id: req.params.room_id};
-  // console.log(target);
-  // db.Reviews.find(target)
-  //   .then((data) => {
-  //     mongoose.connection.close();
-  //     res.status(200).send(data);
-  //   })
-  //   .catch((err) => {
-  //     mongoose.connection.close();
-  //     res.status(500).send("Fail to fetch");
-  //   })
-// });
+app.get('/reviews/:room_id', (req, res) => {
+  mongoose.connect('mongodb://localhost:27017/airbnb', { useNewUrlParser: true });
+  const target = {room_id: req.params.room_id};
+  console.log(target);
+  db.Reviews.find(target)
+    .then((data) => {
+      mongoose.connection.close();
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      mongoose.connection.close();
+      res.status(500).send("Fail to fetch");
+    })
+});
 
 // Route for PostGres
-// app.route('/reviews/:id')
 
 app.route('/reviews/:room_id')
     .get((req, res) => {
